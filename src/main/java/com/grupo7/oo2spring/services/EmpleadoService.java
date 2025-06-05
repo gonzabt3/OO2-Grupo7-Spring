9a5c5aff0daf25b41fdd303e96d642365f30965a
pauche-arg <pauloargibay@gmail.com>
2025-06-05 19:12:59 -0300

    -Fix bug al intentar sacar permisos de un empleado. - Se agrego un estilo para los HTML: listar, formulario_empleado

--- src/main/java/com/grupo7/oo2spring/services/EmpleadoService.java
+++ src/main/java/com/grupo7/oo2spring/services/EmpleadoService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.grupo7.oo2spring.models.Cliente;
 import com.grupo7.oo2spring.models.Empleado;
+import com.grupo7.oo2spring.models.Rol;
+import com.grupo7.oo2spring.models.Usuario;
 import com.grupo7.oo2spring.repositories.IUsuarioRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,24 +19,15 @@ public class EmpleadoService {
 	 private final IUsuarioRepository usuarioRepository;
 	
 	public void sacarPermisosEmpleado(int idEmpleado) throws Exception {
-	    Empleado empleadoAux = usuarioRepository.findEmpleadoById(idEmpleado)
-	        .orElseThrow(() -> new Exception("Empleado no encontrado"));
+		 Usuario usuario = usuarioRepository.findById(idEmpleado)
+			        .orElseThrow(() -> new Exception("Usuario no encontrado"));
 
-	    // Crear cliente con datos del empleado
-	    Cliente cliente = new Cliente(
-	        empleadoAux.getNombre(),
-	        empleadoAux.getApellido(),
-	        empleadoAux.getDni(),
-	        empleadoAux.getEmail(),
-	        empleadoAux.getNombreUsuario(),
-	        empleadoAux.getContraseña()
-	    );
-	    
-	    // Eliminar empleado
-	    usuarioRepository.delete(empleadoAux);
-	    
-	    // Guardar el cliente
-	    usuarioRepository.save(cliente);
+			    // Cambiar el rol
+			    usuario.setRol(Rol.CLIENTE);
+
+
+			    // Guardar el usuario actualizado
+			    usuarioRepository.save(usuario);
 	    
 	}
 
