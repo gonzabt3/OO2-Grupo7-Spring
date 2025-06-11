ea286479d77b3d787244fafbc152ed65eed21fa4
pauche-arg <pauloargibay@gmail.com>
2025-06-10 22:53:38 -0300

    - Se creo una base para la segunda entrega - Se permitieron rutas relacionadas a Swagger a través de SeguridadConfig - Se creo el package controllers.rest para mejor organización entre controladores MVC de Spring y controladores REST - Se creo la clase SwaggerConfig en el package configuration - Se creo AuthRestController, AuthErrorController, LoginDTO - Se modifico EmpleadoDTO para adaptarlo a las necesidades de ManagerRestController - Se modifico LoginController para que funcione como un controlador REST - Se modifico la vista login.html para que funcione con LoginController - Se modifico seguridadConfig para que permita rutas de tipo /api/auth/** - Se agrego la linea org.springframework.web: DEBUG a application.yml para mensajes en la terminal más informativos para debug

--- src/main/java/com/grupo7/oo2spring/seed/DatabaseSeeder.java
+++ src/main/java/com/grupo7/oo2spring/seed/DatabaseSeeder.java
@@ -29,5 +29,17 @@ public class DatabaseSeeder implements CommandLineRunner {
             }
         }
     }
+    
+    private void seedRoles() {
+        if (rolRepository.count() == 0) {
+            Rol[] roles = new Rol[]{
+                new Rol(RoleType.USER),
+                new Rol(RoleType.MANAGER),
+                new Rol(RoleType.EMPLEADO)
+            };
+            rolRepository.saveAll(Arrays.asList(roles));
+        }
+    }
+}
 }
 
