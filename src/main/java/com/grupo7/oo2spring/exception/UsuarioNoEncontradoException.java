f3721e1bd39b8091eaf4218e5165dd85bb413e8e
Alexcadima <alexx.cadima@gmail.com>
2025-06-20 20:14:44 -0300

    Agregado la excepcion Usuario

---
+++ src/main/java/com/grupo7/oo2spring/exception/UsuarioNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.grupo7.oo2spring.exception;
+
+public class UsuarioNoEncontradoException extends Exception{
+	public UsuarioNoEncontradoException(String mensaje) {
+		super(mensaje);
+	}
+
+}
