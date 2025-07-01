a23e3e6e8a324c1b612de1000ebd6c4078885b2c
pauche-arg <pauloargibay@gmail.com>
2025-07-01 19:53:59 -0300

    - Se reemplazo el servicio de mail de Brevo con el de Gmail para cumplir con la consigna

--- src/main/java/com/grupo7/oo2spring/controller/TicketController.java
+++ src/main/java/com/grupo7/oo2spring/controller/TicketController.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
