12a6a0e1454fdea63f33e5e2c9f3e3b2e067fa30
gonza <gonzalo.muscolo@gmail.com>
2025-07-03 17:27:13 -0300

    se agrego el reacord dto a area y excepcion si no puede hacer el delete

--- src/main/java/com/grupo7/oo2spring/controller/rest/AreaController.java
+++ src/main/java/com/grupo7/oo2spring/controller/rest/AreaController.java
@@ -1,8 +1,11 @@
 package com.grupo7.oo2spring.controller.rest;
 
+import com.grupo7.oo2spring.controller.rest.dto.AreaDTO;
 import com.grupo7.oo2spring.models.Area;
 import com.grupo7.oo2spring.services.AreaService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,20 +17,21 @@ public class AreaController {
 
     private final AreaService areaService;
 
-    @GetMapping
-    public List<Area> listarAreas() {
-        return areaService.listarAreas();
-    }
-
     @GetMapping("/nombre/{nombre}")
-  public Area getAreaByName(@PathVariable String nombre) {
-      return areaService.buscarPorNombre(nombre)
-              .orElseThrow(() -> new RuntimeException("Área no encontrada: " + nombre));
-  }
-
+    public AreaDTO getAreaByName(@PathVariable String nombre) {
+        Area area = areaService.buscarPorNombre(nombre)
+                .orElseThrow(() -> new RuntimeException("Área no encontrada: " + nombre));
+        return new AreaDTO(area.getId(), area.getTipo().nombre);
+    }
+    
 
     @DeleteMapping("/{id}")
-    public void eliminarArea(@PathVariable int id) {
-        areaService.eliminarArea(id);
+    public ResponseEntity<?> eliminarArea(@PathVariable int id) {
+        try {
+            areaService.eliminarArea(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body("No se pudo eliminar el área");
+        }
     }
 }
