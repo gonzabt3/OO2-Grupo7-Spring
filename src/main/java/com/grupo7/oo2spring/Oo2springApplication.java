aeb1c77211121b9a5a762b4fc59a9c02e3e5eb2a
gonza <gonzalo.muscolo@gmail.com>
2025-07-03 02:31:34 -0300

    agrego seeder para area

--- src/main/java/com/grupo7/oo2spring/Oo2springApplication.java
+++ src/main/java/com/grupo7/oo2spring/Oo2springApplication.java
@@ -1,10 +1,13 @@
 package com.grupo7.oo2spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import com.grupo7.oo2spring.seed.DatabaseSeeder;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.grupo7.oo2spring")
