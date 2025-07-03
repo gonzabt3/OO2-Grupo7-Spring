e85c34d9fc7acc8b14770b52ecad9232b34072b9
gonza <gonzalo.muscolo@gmail.com>
2025-07-03 17:27:13 -0300

    se agrego el reacord dto a area y excepcion si no puede hacer el delete

--- src/main/java/com/grupo7/oo2spring/controller/rest/AreaController.java
+++ src/main/java/com/grupo7/oo2spring/controller/rest/AreaController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import com.grupo7.oo2spring.models.Area;
 import com.grupo7.oo2spring.services.AreaService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,12 +35,6 @@ public class AreaController {
         return areaService.listarAreas();
     }
 
-    @GetMapping("/nombre/{nombre}")
-  public Area getAreaByName(@PathVariable String nombre) {
-      return areaService.buscarPorNombre(nombre)
-              .orElseThrow(() -> new RuntimeException("Área no encontrada: " + nombre));
-  }
-    
     @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarArea(@PathVariable int id) {
         try {
@@ -47,9 +43,5 @@ public class AreaController {
         } catch (Exception e) {
             return ResponseEntity.status(404).body("No se pudo eliminar el área");
         }
-
-    @DeleteMapping("/{id}")
-    public void eliminarArea(@PathVariable int id) {
-        areaService.eliminarArea(id);
     }
 }
