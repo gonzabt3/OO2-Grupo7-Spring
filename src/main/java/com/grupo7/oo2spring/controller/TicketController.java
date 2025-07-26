31d89b5f9b952027b857cf2b34b29c7516c750b4
Alexcadima <44731290+Alexcadima@users.noreply.github.com>
2025-07-26 00:39:03 -0300

    Update TicketController.java

--- src/main/java/com/grupo7/oo2spring/controller/TicketController.java
+++ src/main/java/com/grupo7/oo2spring/controller/TicketController.java
@@ -1,218 +1,171 @@
-package com.grupo7.oo2spring.controller;
+package com.grupo7.oo2spring.services;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.time.LocalDateTime;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Service;
 
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.grupo7.oo2spring.dto.ControlDTO;
-import com.grupo7.oo2spring.dto.TicketDTO;
-import com.grupo7.oo2spring.enums.TipoArea;
+import com.grupo7.oo2spring.models.Ticket;
+import com.grupo7.oo2spring.models.Usuario;
 import com.grupo7.oo2spring.exception.TicketCreacionException;
 import com.grupo7.oo2spring.exception.TicketNoEncontradoException;
 import com.grupo7.oo2spring.models.Area;
+import com.grupo7.oo2spring.dto.ControlDTO;
+import com.grupo7.oo2spring.dto.TicketDTO;
+import com.grupo7.oo2spring.enums.TipoArea;
 import com.grupo7.oo2spring.models.Control;
-import com.grupo7.oo2spring.models.Ticket;
-import com.grupo7.oo2spring.models.Usuario;
-import com.grupo7.oo2spring.models.UsuarioBase;
 import com.grupo7.oo2spring.models.Empleado;
 import com.grupo7.oo2spring.models.Estado;
+import com.grupo7.oo2spring.models.Funcion;
 import com.grupo7.oo2spring.models.Prioridad;
+import com.grupo7.oo2spring.repositories.IAreaRepository;
+import com.grupo7.oo2spring.repositories.IControlRepository;
 import com.grupo7.oo2spring.repositories.ITicketRepository;
-import com.grupo7.oo2spring.repositories.IUsuarioRepository;
-import com.grupo7.oo2spring.security.UsuarioDetails;
-import com.grupo7.oo2spring.services.AreaService;
-import com.grupo7.oo2spring.services.EmailService;
-import com.grupo7.oo2spring.services.EmpleadoService;
-import com.grupo7.oo2spring.services.TicketService;
-import com.grupo7.oo2spring.services.UsuarioService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+import com.grupo7.oo2spring.repositories.IUsuarioRepository;
+
+@Service
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/ticket")
-public class TicketController {
-	
+public class TicketService {
+
 	private final ITicketRepository ticketRepository;
-    
-    private final TicketService ticketService;
 
-    private final EmpleadoService empleadoService;
-    
-    private final UsuarioService usuarioService;
+	private final IUsuarioRepository usuarioRepository;
 
-    private final AreaService areaService;
-    
+	private final IControlRepository controlRepository;
 
-    @GetMapping("/formulario_simple")
-    public String mostrarFormulario(Model model) {
-        model.addAttribute("ticket", new TicketDTO());
-        return "ticket/formulario_simple";
-    }
+	private final AreaService areaService;
+	private final IAreaRepository areaRepository;
 
-    @PostMapping("/crear")
-    public String crearTicket(@ModelAttribute("ticket") TicketDTO ticketDTO, RedirectAttributes redirectAttributes,
-                              @AuthenticationPrincipal UserDetails usuariolog) {
-        System.out.println("CONTROLADOR: Recibido POST /crear con ticketDTO: " + ticketDTO);
 
-        if (usuariolog == null) {
-            System.out.println("CONTROLADOR: Usuario no autenticado");
-            redirectAttributes.addFlashAttribute("mensaje", "Error con el Usuario");
-            return "redirect:/panel.html";
-        }
+	public Ticket getByIdTicket(int idTicket) {
+		return ticketRepository.getByIdTicket(idTicket);
+	}
 
-        String nombreDelUsuarioEnSesion = usuariolog.getUsername();
-        System.out.println("CONTROLADOR: Usuario en sesión: " + nombreDelUsuarioEnSesion);
-
-        Usuario usuarioCreador = usuarioService.getUsuarioByNombreUsuario(nombreDelUsuarioEnSesion);
-        System.out.println("CONTROLADOR: Usuario creador obtenido: " + usuarioCreador);
-
-        try {
-            Ticket ticketCreado = ticketService.crearTicket(ticketDTO, usuarioCreador); 
-            System.out.println("CONTROLADOR: Ticket creado con ID: " + ticketCreado.getIdTicket());
-            redirectAttributes.addFlashAttribute("successMessage", "¡Ticket exitoso! Ya puedes iniciar sesión.");
-            return "redirect:/ticket/exito";
-        } catch (RuntimeException e) {
-            System.err.println("CONTROLADOR: Error al registrar ticket: " + e.getMessage());
-            redirectAttributes.addFlashAttribute("error", e.getMessage());
-            redirectAttributes.addFlashAttribute("ticket", ticketDTO);
-            return "redirect:/formulario-ticket";
-        }
-    }
- 
-
-    @GetMapping("/exito")
-    public String mostrarExito(Model model) {
-        model.addAttribute("mensajeExito", "¡El ticket ha sido creado con éxito!");
-        return "ticket/exito";
-    }
-	
-	@GetMapping("/listaArea")
-	@PreAuthorize("hasRole('EMPLEADO')")
-	public String listarticketsPorArea(Model model, @AuthenticationPrincipal UserDetails usuariolog){
-		model.addAttribute("usuarioLogueado", usuariolog);
-		List<Ticket> tickets = null;
-		Empleado empleadoOpt = empleadoService.findByNombreUsuario(usuariolog.getUsername());
-		if(empleadoOpt.getArea().getTipo() != null) {
-			tickets = ticketService.findByArea_Tipo(empleadoOpt.getArea().getTipo());
-			model.addAttribute("message", "Mostrando solo tickets de su área: " + empleadoOpt.getArea());
-			model.addAttribute("tickets", tickets);
-		}else {
-			model.addAttribute("message", "No existen tickets asignados a su Area ");
-		}
-		model.addAttribute("estados", Arrays.asList(Estado.values()));
-        model.addAttribute("prioridades", Arrays.asList(Prioridad.values()));
-		model.addAttribute("tickets", tickets);
-		return "ticket/lista_tickets";
-    }
-	
-	@GetMapping("/{idTicket}/detalle")
-    public String DetalleTicket(@PathVariable int idTicket, Model model, @AuthenticationPrincipal UserDetails usuariolog) throws TicketNoEncontradoException {
-        Ticket ticketDetalle = ticketService.buscarTicketPorId(idTicket);
-        model.addAttribute("ticketDetalle", ticketDetalle);
-        return "ticket/ticket-detalle";
-    }
-	
-	
-	@PreAuthorize("hasAnyRole('MANAGER')")
-	@GetMapping("/lista")
-    public String ticketsDelSistema(Model model, @AuthenticationPrincipal UserDetails usuariolog) {
-		UsuarioDetails usuarioDetails = (UsuarioDetails)usuariolog;
-		UsuarioBase empleado = usuarioDetails.getUsuario();
-        try {
-            List<Ticket> tickets = ticketRepository.findAll();
-            model.addAttribute("tickets", tickets);
-            model.addAttribute("rol",empleado.getRol());
-            model.addAttribute("areas", areaService.listarAreas());
-            model.addAttribute("estados", Estado.values());
-            model.addAttribute("prioridades", Prioridad.values());
-            return "ticket/ticket_del_sistema"; 
-        } catch (RuntimeException e) {
-            model.addAttribute("errorMessage", e.getMessage());
-            return "redirect:/ticket/ticket_del_sistema"; 
-        }
-    }
-	
-
-	@PostMapping("/{idTicket}/asignarArea")
-	@PreAuthorize("hasAnyRole('MANAGER')")
-	public String asigarUnArea(@PathVariable int idTicket,
-            @RequestParam("area") Area area,
-            RedirectAttributes redirectAttributes) throws TicketNoEncontradoException {
-		ticketService.asignarAreaTicket(idTicket, area);
-		//redirectAttributes.addFlashAttribute("successMessage", "¡Área '" + area.getTipo().getNombre() + "' asignada al ticket #" + idTicket + " con éxito!");
-		redirectAttributes.addFlashAttribute("successMessage", "¡Área '" + area.getTipo().nombre + "' asignada al ticket #" + idTicket + " con éxito!");
-		return "redirect:/ticket/lista";
+	@Transactional(readOnly = true)
+	public List<Ticket> findByAreaIsNull() {
+		return ticketRepository.findByAreaIsNull();
 	}
 	
-	@PreAuthorize("hasAnyRole('USER')")
-	@GetMapping("/tickets")
-	public String verTicketsUsuario(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-	    if (userDetails == null) {
-	        return "redirect:/login";
+	@Transactional(readOnly = true)
+	public List<Ticket> findByUsuario(Usuario usuario) {
+		return ticketRepository.findByUsuarioCreador(usuario);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Ticket> findByArea_Tipo(TipoArea area) {
+		return ticketRepository.findByArea_Tipo(area);
+	}
+
+	@Transactional
+	public Ticket crearTicket(TicketDTO ticket, Usuario usuarioCreador) {
+		System.out.println("SERVICIO: Creando ticket con DTO: " + ticket);
+		
+		try {
+			Ticket nuevoTicket = new Ticket(ticket.getTitulo(), ticket.getDescripcion(), usuarioCreador);
+			Optional<Area> areaOpt = areaRepository.findByTipo(TipoArea.SIN_ASIGNAR);
+			Area areaSinAisgnar = areaOpt.get();
+			nuevoTicket.setArea(areaSinAisgnar);
+			Ticket guardado = ticketRepository.save(nuevoTicket);
+			System.out.println("SERVICIO: Ticket guardado con ID: " + guardado.getIdTicket());
+			return guardado;
+		} catch (Exception e) {
+			throw new TicketCreacionException("Error al guardar el ticket: " + e.getMessage());
+		}
+	}
+
+	@Transactional
+	public void tomarTicketConControlInicial(int idTicket, Empleado empleadoLogueado, ControlDTO control)
+			throws Exception {
+		try {
+		Ticket ticket = ticketRepository.getByIdTicket(idTicket);
+
+		Control controlInicial = new Control();
+		controlInicial.setTicket(ticket);
+		controlInicial.setAccion(control.accion());
+		controlInicial.setFechaEntrada(LocalDateTime.now());
+		controlInicial.setFinalizado(false); // No está finalizado al tomarlo
+		controlInicial.setFechaSalida(null);
+		controlInicial.setFuncion(Funcion.valueOf(control.funcion().toUpperCase()));
+		
+		controlInicial.setEmpleado(empleadoLogueado);
+		ticket.setEstado(Estado.ABIERTO);
+		ticket.addControl(controlInicial);
+
+
+		controlRepository.save(controlInicial);
+		ticketRepository.save(ticket);
+		System.out.println("Ticket #" + idTicket + " tomado exitosamente por " + empleadoLogueado.getNombre() + " "
+				+ empleadoLogueado.getApellido());
+	}catch (Exception e) {
+		 System.out.println("❌ Error al tomar ticket en el service:");
+	        e.printStackTrace();
+	        throw new RuntimeException("Falló tomarTicketConControlInicial: " + e.getMessage(), e);
+		}
+	}
+	
+	 public Ticket buscarTicketPorId(int idTicket) throws TicketNoEncontradoException {
+	        return ticketRepository.findById(idTicket)
+	            .orElseThrow(() -> new TicketNoEncontradoException("Ticket con ID " + idTicket + " no encontrado"));
+	    }
+	
+	 @Transactional
+	 public Ticket asignarAreaTicket(int idTicket, Area area) throws TicketNoEncontradoException {
+	     Ticket ticket = ticketRepository.getByIdTicket(idTicket);
+	     ticket.setArea(area);
+	     return ticketRepository.save(ticket);
+
+	 }
+	 @Transactional
+	    public Ticket asignarPrioridad(int ticketId, Prioridad nuevaPrioridad) throws Exception {
+	        Ticket ticket = ticketRepository.findById(ticketId)
+	                .orElseThrow(() -> new Exception("Error al recuperar datos del Ticket existente"));
+	
+	        ticket.setPrioridad(nuevaPrioridad);
+	        return ticketRepository.save(ticket); // Guarda los cambios en la base de datos
+	    }
+	 @Transactional
+	    public Ticket asignarEstado(int ticketId, Estado nuevoEstado) throws Exception {
+	        Ticket ticket = ticketRepository.findById(ticketId)
+	                .orElseThrow(() -> new Exception("Error al recuperar datos del Ticket existente"));
+
+	        ticket.setEstado(nuevoEstado);
+	        return ticketRepository.save(ticket);
+	 }
+	 @Transactional
+	    public void delete(int id) throws TicketNoEncontradoException {
+	        if (!ticketRepository.existsById(id)) {
+	            throw new TicketNoEncontradoException("Ticket con ID " + id + " no encontrado");
+	        }
+	        ticketRepository.deleteById(id);
 	    }
 
-	    String username = userDetails.getUsername();
-	    Usuario usuario = usuarioService.getUsuarioByNombreUsuario(username);
-
-	    List<Ticket> tickets = ticketService.getTicketsByUsuario(usuario.getId());
-	    model.addAttribute("tickets", tickets);
-
-	    return "ticket/usuario-tickets"; // Vista con la tabla de tickets
+	public List<Ticket> getTicketsByUsuario(int usuarioIdCreador) {
+		return ticketRepository.findByUsuarioCreador_Id(usuarioIdCreador);
 	}
 	
-	@PreAuthorize("hasAnyRole('MANAGER', 'EMPLEADO')")
-	@PostMapping("/{idTicket}/cambiarPrioridad")
-	public String cambiarPrioridad(@PathVariable int idTicket, @RequestParam("prioridad")Prioridad prioridad, Model model) throws Exception {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		ticketService.asignarPrioridad(idTicket, prioridad);
-		if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_MANAGER"))) {
-            return "redirect:/ticket/lista";
-        }
-        else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_EMPLEADO"))) {
-            return "redirect:/ticket/listaArea";
-        }
-		return "redirect:/panel.html";
-	}
-	@PreAuthorize("hasAnyRole('MANAGER', 'EMPLEADO')")
-	@PostMapping("/{idTicket}/cambiarEstado")
-	public String cambiarEstado(@PathVariable int idTicket, @RequestParam("estado")Estado estado, Model model) throws Exception {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		ticketService.asignarEstado(idTicket, estado);
-		if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_MANAGER"))) {
-            return "redirect:/ticket/lista";
-        }
-        else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_EMPLEADO"))) {
-            return "redirect:/ticket/listaArea";
-        }
-		return "redirect:/panel.html";
-	}
-	
-	//Para testear la excepcion
-    @GetMapping("/probar-exception")
-    public String probarError() throws TicketNoEncontradoException {
-        throw new TicketNoEncontradoException("No se encontró el ticket solicitado.");
-    }
+	 @Transactional
+	    public Ticket update(int id, Ticket ticketActualizado) throws TicketNoEncontradoException {
+	        Ticket ticketExistente = ticketRepository.findById(id)
+	            .orElseThrow(() -> new TicketNoEncontradoException("Ticket con ID " + id + " no encontrado"));
+
+	        // Actualiza solo los campos que se pueden modificar
+	        ticketExistente.setTitulo(ticketActualizado.getTitulo());
+	        ticketExistente.setDescripcion(ticketActualizado.getDescripcion());
+	        ticketExistente.setPrioridad(ticketActualizado.getPrioridad());
+	        ticketExistente.setEstado(ticketActualizado.getEstado());
+	        ticketExistente.setArea(ticketActualizado.getArea());
+	        // Agrega aquí otros campos editables si corresponde
+
+	        return ticketRepository.save(ticketExistente);
+	    }
 }
-
